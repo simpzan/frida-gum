8fdccbbbed77e038970b90a93ee093f7d753eaf7
Samuel Zhang <simpzan@gmail.com>
2022-03-06 19:15:43 +0800

    fix trace-cmd.js error on amd64.

--- trace/trace-cmd.js
+++ trace/trace-cmd.js
@@ -33,13 +33,14 @@ function runCmd(cmd) {
 }
 
 function getFunctionMaps() {
-    // runCmd('./trace-cmd/tracecmd/trace-cmd report -tR | grep trace-cmd-8500 > ./trace.txt');
-    // runCmd('./trace-cmd/tracecmd/trace-cmd report -f > ./fns.txt');
+    // runCmd('trace-cmd report -tR | grep trace-cmd-8500 > ./trace.txt');
+    runCmd('trace-cmd report -f > ./fns.txt');
     const out = fs.readFileSync('./fns.txt', 'utf8');
     const functions = {};
     out.split('\n').forEach(line => {
         const parts = line.split(' ');
-        functions[parts[0]] = parts;
+        const addr = parseInt(parts[0], 16) + 4;
+        functions[addr] = parts;
     });
     return functions;
 }
@@ -90,12 +91,15 @@ async function processLineByLine(file) {
     }
     const name = m[1];
     const tid = parseInt(m[2], 10);
-    const ts = parseFloat(m[3]) * 1000000;
+    const ts = parseInt(m[3], 10) / 1000;
     const phase = m[4];
-    const fnAddr = m[5];
+    const fnAddr = parseInt(m[5], 16);
     const stacktrace = stacktraceByTid[tid] = stacktraceByTid[tid] || new Stacktrace(tid, name);
     const fn = functions[fnAddr];
-    log(phase, fn);
+    if (!fn) {
+        log(phase, fn, m);
+        continue;
+    }
     const event = stacktrace.addEvent(ts, fn[1], phase==='entry');
     ctf.writeObject(event);
   }
@@ -105,5 +109,6 @@ async function processLineByLine(file) {
 async function main() {
     const file = './trace.txt';
     await processLineByLine(file);
+    runCmd('zip -r t.zip t.json');
 }
 main();
